7b6090117ecd9f39a15cf33e24590c68ef3cc62b
tvieregge <tmvieregge@gmail.com>
2019-05-20 19:18:55 -0400

    to orbit

--- mousetoy.c
+++ mousetoy.c
@@ -3,27 +3,32 @@
 #include <X11/extensions/XInput2.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+typedef struct PhysicsEnt {
+    double vx, vy;
+    double x, y;
+    int id;
+} PhysicsEnt;
 
 typedef struct Context {
     Display *display;
     Window root_window;
 } Context;
 
-void warp(Context context, int deviceid, int x, int y) {
+void warp(Context context, int deviceid, double x, double y) {
 
-    int src_x = 0;
-    int src_y = 0;
+    double src_x = 0;
+    double src_y = 0;
     int src_width = 0;
     int src_height = 0;
-
-    int dest_x = 100;
-    int dest_y = 100;
 
     Window dest_w = context.root_window;
     Window src_w = None;
 
     int err = XIWarpPointer(context.display, deviceid, src_w, dest_w, src_x,
-                            src_y, src_width, src_height, x, dest_y);
+                            src_y, src_width, src_height, x, y);
     XFlush(context.display);
 }
 
@@ -42,7 +47,51 @@ void query(Context context, int deviceid, double *x, double *y) {
                    &modifier_group_dummy);
 }
 
+void loop(Context context, PhysicsEnt c1, PhysicsEnt c2) {
+    double fy, fx;
+    double c = 50.0;
+
+    while(True) {
+        query(context, c1.id, &c1.x, &c1.y);
+        query(context, c2.id, &c2.x, &c2.y);
+
+        // calculate forces
+        double xdiff = c1.x - c2.x;
+        double ydiff = c1.y - c2.y;
+        double d = sqrt(xdiff*xdiff + ydiff*ydiff);
+        if(d < 5) {
+            d = 5;
+        }
+        double f = c * (1.0/ (pow(d, 1.5) ));
+        fx = xdiff/d * f;
+        fy = ydiff/d * f;
+
+        // calculate new positions
+        c1.vx += -fx;
+        c2.vx += fx;
+        c1.vy += -fy;
+        c2.vy += fy;
+
+        printf("%f\n", c1.y);
+        c1.x += c1.vx;
+        c2.x += c2.vx;
+        c1.y += c1.vy;
+        c2.y += c2.vy;
+
+        // move cursors
+        warp(context, c1.id, c1.x, c1.y);
+        warp(context, c2.id, c2.x, c2.y);
+
+        // sleep
+        usleep(30000);
+    }
+}
+
 int main() {
+    int first_id = 2;
+    int second_id = 16;
+    double x,y;
+
     char *env_display = getenv("DISPLAY");
     Display *display = XOpenDisplay(env_display);
 
@@ -54,12 +103,13 @@ int main() {
     context.display = display;
     context.root_window = root_window;
 
-    double x, y;
-
-    int first_id = 2;
-    int second_id = 16;
+    query(context, first_id, &x, &y);
+    PhysicsEnt c1 = {0, 0, x, y, first_id};
 
     query(context, second_id, &x, &y);
-    // warp(context, 14, 100, 100);
+    PhysicsEnt c2 = {0, 0, x, y, second_id};
+
     printf("%f %f\n", x, y);
+
+    loop(context, c1, c2);
 }
